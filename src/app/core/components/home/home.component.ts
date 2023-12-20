1e23f6f379bcbf5795bc6381f788c9c15ccf8574
Harold Diaz <harold.diazv1@gmail.com>
2023-12-20 11:14:32 -0500

    Create public component for auth all users

---
+++ src/app/core/components/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-public',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {
+  @ViewChild('f') form!: NgForm;
+
+  submissionType: 'login' | 'join' = 'login';
+
+  toggleText() {
+    this.submissionType = this.submissionType === 'login' ? 'join' : 'login';
+  }
+}
