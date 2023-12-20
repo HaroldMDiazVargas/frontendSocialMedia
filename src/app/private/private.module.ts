497c1a8b5b29ed099cfcf02b6ef75c667753dfcc
Harold Diaz <harold.diazv1@gmail.com>
2023-12-20 16:48:26 -0500

    Set route and component for all posts and create new one

--- src/app/private/private.module.ts
+++ src/app/private/private.module.ts
@@ -4,9 +4,21 @@ import { HeaderComponent } from './components/header/header.component';
 import { PrivateComponent } from './private.component';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from './components/header/sidebar/sidebar.component';
+import { AllPostsComponent } from './components/all-posts/all-posts.component';
+import { StartPostComponent } from './components/start-post/start-post.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { PrivateRoutingModule } from './private-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [HeaderComponent, PrivateComponent, SidebarComponent],
-  imports: [CommonModule, RouterModule],
+  declarations: [
+    HeaderComponent,
+    PrivateComponent,
+    SidebarComponent,
+    AllPostsComponent,
+    StartPostComponent,
+    FeedComponent,
+  ],
+  imports: [CommonModule, RouterModule, PrivateRoutingModule, FormsModule],
 })
 export class PrivateModule {}
